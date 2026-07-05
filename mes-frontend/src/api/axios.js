c3839bb70bf5e2531d5a27a8f4b9f2e60907c6c9
kathiriyaPrince <princekathiriya065@gmail.com>
2026-07-06 01:14:02 +0200

    Update API URL for production

--- mes-frontend/src/api/axios.js
+++ mes-frontend/src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// export const api = axios.create({
+//   baseURL: 'http://localhost:5000/api'
+// });
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://mes-backend-api.onrender.com/api'
 });
 
 api.interceptors.request.use(config => {
